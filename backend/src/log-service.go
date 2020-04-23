73cb19dff625912ed6b4dd492b345066c1c899e3
David Marquez <davidmarquezherreros@gmail.com>
2020-04-23 08:30:11 +0200

    Added logging service

--- backend/src/log-service.go
+++ backend/src/log-service.go
@@ -1,5 +1,25 @@
 package main
 
-func Log() {
-	print("HELLO WORLD")
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func RegisterLog(error string, failedDb bool) {
+
+	error = fmt.Sprintf("ERROR: %s				%s", error, time.Now().String())
+
+	if configuration == nil {
+		configuration = InitializeConfiguration()
+	}
+
+	if configuration.Logging.Database && !failedDb {
+		parameters := []string{error}
+		ExecuteSPWithParametersNoReturn("SP_INSERT_LOG_BACKEND", parameters)
+	} else {
+		f, _ := os.OpenFile(configuration.Logging.Filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+		f.WriteString(error)
+		f.Close()
+	}
 }
