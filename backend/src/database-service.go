98a4a146266b7b7ccf3f9305f262fa9480dcb378
David Marquez <davidmarquezherreros@gmail.com>
2020-04-24 19:00:17 +0200

    Refactored database-service

--- backend/src/database-service.go
+++ backend/src/database-service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -59,17 +60,34 @@ func ExecuteSPWithParametersNoReturn(spname string, parameters []string) {
 
 	db, err = sql.Open("mysql", GetConnectionString())
 
-	var sqlStatement string = fmt.Sprintf("CALL `%s`(", spname)
-
-	for i := 0; i < len(parameters); i++ {
-		sqlStatement += fmt.Sprintf("'%s'", parameters[i])
-	}
-	sqlStatement += ")"
-	_, err := db.Exec(sqlStatement)
+	_, err := db.Exec(BuildSqlStatement(spname, parameters))
 
 	if err != nil {
 		RegisterLog(err.Error(), true)
 	}
 
 	defer db.Close()
+}
+
+///
+/// Executes a defined stored procedure with parameters
+/// Does return records
+///
+func ExecuteSPWithParametersWithReturn(spname string, parameters []string) *sql.Rows {
+
+	db, err = sql.Open("mysql", GetConnectionString())
+
+	rows, err := db.Query(BuildSqlStatement(spname, parameters))
+
+	if err != nil {
+		RegisterLog(err.Error(), true)
+	}
+
+	defer db.Close()
+	return rows
+}
+
+func BuildSqlStatement(spname string, parameters []string) string {
+
+	return fmt.Sprintf("CALL `%s`( %s )", spname, strings.Join(parameters, ","))
 }
